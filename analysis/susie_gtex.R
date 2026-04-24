06f9d90d9c21a56adb81a0064ec7735c5ed555a0
Peter Carbonetto <peter.carbonetto@gmail.com>
2026-04-24 15:31:26 -0500

    Added steps to prepare the phenotypes in susie_gtex.R.

--- analysis/susie_gtex.R
+++ analysis/susie_gtex.R
@@ -4,6 +4,9 @@
 #
 library(data.table)
 library(susieR) # 0.14.2
+set.seed(1)
+target_gene   <- "CBX8"
+target_tissue <- "Lung"
 
 # Read in the covariate data.
 cov1 <- read.table("GTEx_Analysis_v8_Annotations_SubjectPhenotypesDS.txt.gz",
@@ -14,19 +17,38 @@ cov2 <- transform(cov2,SUBJID = substr(SAMPID,1,10))
 cov <- merge(cov1,cov2,by = "SUBJID")
 cov <- cov[c("SUBJID","SAMPID","SEX","AGE","SMTS","SMGEBTCHT","SMTSD")]
 cov <- transform(cov,
-                 SEX       = factor(SEX),
+                 SEX       = SEX - 1,
                  AGE       = factor(AGE),
                  SMTS      = factor(SMTS),
+                 SMTSD     = factor(SMTSD),
                  SMGEBTCHT = factor(SMGEBTCHT))
-
-stop()
+rownames(cov) <- cov$SAMPID
 
 # Read in the gene expression data.
 pheno <- fread("GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct.gz",
                sep = "\t",skip = 2,header = TRUE,showProgress = TRUE)
 class(pheno) <- "data.frame"
+gene_info <- pheno[1:2]
+pheno <- pheno[-(1:2)]
+pheno <- as.matrix(pheno)
+pheno <- t(pheno)
+storage.mode(pheno) <- "double"
+colnames(pheno) <- gene_info$Name
 
-# TO DO. Save the fine-mapping data in an .RData file.
+# Align the gene expression and covariate data.
+ids   <- intersect(cov$SAMPID,rownames(pheno))
+rows1 <- match(ids,cov$SAMPID)
+rows2 <- match(ids,rownames(pheno))
+cov   <- cov[rows1,]
+pheno <- pheno[rows2,]
+
+# Extract the gene expression data for the target gene.
+j <- which(gene_info$Description == target_gene)
+pheno <- cbind(cov,data.frame(count = pheno[,j]))
+
+# Extract the data for the target tissue.
+pheno <- subset(pheno,SMTS == target_tissue)
+pheno <- transform(pheno,SMGEBTCHT = factor(SMGEBTCHT))
 
 # WGS sequencing platform (HiSeq 2000 or HiSeq X), WGS library
 # construction protocol (PCR-based or PCR-free) and donor sex were
@@ -35,3 +57,10 @@ class(pheno) <- "data.frame"
 #
 # Expression values for each gene were inverse normal transformed
 # across samples.
+pheno$y <- resid(lm(count ~ SEX,pheno))
+
+# TO DO NEXT: Read in the genotype data for SNPs near the target gene.
+# Use PLINK to automate this.
+
+# Save the fine-mapping data in an .RData file.
+# TO DO.
