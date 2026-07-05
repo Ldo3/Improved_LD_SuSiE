ec1c75f989a643b4fa77c389fce4e5c76ef845c9
Peter Carbonetto <peter.carbonetto@gmail.com>
2026-07-04 20:54:48 -0500

    Added susie runs with L=2.

--- analysis/susie_residuals.R
+++ analysis/susie_residuals.R
@@ -1,59 +1,3 @@
-# remotes::install_github("stephenslab/susieR@output_residuals")
-library(susieR)
-library(matrixStats)
-print(packageVersion("susieR"))
-set.seed(1)
-p1      <- 2
-min_maf <- 0.03
-n_out   <- 100
-
-# Load the genotype data.
-X <- readRDS("../data/Thyroid.FMO2.1Mb.RDS")$X
-storage.mode(X) <- "double"
-X <- X[,1:1000]
-
-# Filter the SNPs by MAF.
-maf <- colMeans(X)/2
-maf <- pmin(maf,1 - maf)
-j   <- which(maf > min_maf)
-X   <- X[,j]
-print(dim(X))
-
-# Choose the causal SNPs.
-p <- ncol(X)
-j <- sample(p,p1)
-
-# Simulate b.
-b        <- rep(0,p)
-names(b) <- colnames(X)
-b[j]     <- sample(c(-1,1),p1,replace = TRUE)
-
-# Simulate y.
-n <- nrow(X)
-e <- rnorm(n,sd = 0.85)
-y <- drop(X %*% b + e)
-y <- y/sd(y)
-
-# Compute the "in-sample" LD matrix and an "out-of-sample" LD matrix.
-R <- cor(X)
-i <- sample(n,n_out)
-R_out <- cor(X[i,])
-
-# Compute the summary statistics.
-out  <- univariate_regression(X,y)
-zhat <- with(out,betahat/sebetahat)
-
-# Run susie_rss with the "in-sample" LD matrix.
-fit <- susie_rss(zhat,R,L = 4,n = n,estimate_prior_method = "EM",
-                 tol = 1e-6,min_abs_corr = 0,verbose = TRUE)
-par(mfrow = c(1,2))
-susie_plot(fit,y = "PIP",b = (b != 0))
-
-# Run susie_rss with the "out-of-sample" LD matrix.
-fit_out <- susie_rss(zhat,R_out,L = 4,n = n,estimate_prior_method = "EM",
-                     tol = 1e-6,min_abs_corr = 0,verbose = TRUE)
-susie_plot(fit_out,y = "PIP",b = (b != 0))
-
 A <- tcrossprod(zhat)
 A <- A/max(abs(A))
 w <- 1
@@ -61,9 +5,3 @@ w <- 1
 R2 <- w*R_out + (1-w)*A
 fit_out <- susie_rss(zhat,R2,L = 4,n = n,estimate_prior_method = "EM",
                      tol = 1e-6,min_abs_corr = 0,verbose = TRUE)
-
-par(mfrow = c(1,2))
-susie_plot(fit,y = "PIP",b = (b != 0))
-susie_plot(fit_out,y = "PIP",b = (b != 0))
-hist(fit$XtR,breaks = 64)
-hist(fit_out$XtR,breaks = 64)
